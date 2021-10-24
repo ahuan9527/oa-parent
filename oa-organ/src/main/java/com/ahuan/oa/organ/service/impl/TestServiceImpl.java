d6cde20511f5d0378d48d27085c45cf2a99e9a6f
Jhuan <a1875884755@gmail.com>
2021-10-24 16:15:32 +0800

    testservice invoke submit!

--- oa-organ/src/main/java/com/ahuan/oa/organ/service/impl/TestServiceImpl.java
+++ oa-organ/src/main/java/com/ahuan/oa/organ/service/impl/TestServiceImpl.java
@@ -5,5 +5,6 @@ import com.ahuan.oa.organ.service.TestService;
 public class TestServiceImpl implements TestService {
     public void sayHello() {
         System.out.println("你好！");
+        System.out.println("pring invoke this method !");
     }
 }
