5ec937af29810160277df0e0aa4018a142007d7c
Jhuan <a1875884755@gmail.com>
2021-10-24 21:03:17 +0800

    bugfix is repair!

--- oa-organ/src/main/java/com/ahuan/oa/organ/service/impl/TestServiceImpl.java
+++ oa-organ/src/main/java/com/ahuan/oa/organ/service/impl/TestServiceImpl.java
@@ -4,7 +4,7 @@ import com.ahuan.oa.organ.service.TestService;
 
 public class TestServiceImpl implements TestService {
     public void sayHello() {
-        System.out.println("你好！");
+        System.out.println("你好！,bug 修复！");
         System.out.println("pring invoke this method !");
         System.out.println("打印一个好的日志！");
     }
